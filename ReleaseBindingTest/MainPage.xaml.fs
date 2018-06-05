4b10a34cef47db7160cc6529baf509c5c1acb294
Sam Williams <>
2018-06-06 06:49:15 +1200

    init

---
+++ ReleaseBindingTest/MainPage.xaml.fs
@@ -0,0 +1,29 @@
+﻿namespace ReleaseBindingTest
+
+open Xamarin.Forms
+open Xamarin.Forms.Xaml
+open FSharp.Data
+open System.Diagnostics
+
+type MainPage() =
+    inherit ContentPage()
+    let _ = base.LoadFromXaml(typeof<MainPage>)
+
+    let clickMe = base.FindByName<Button>("ClickMe")
+
+
+    let apiLookup () = 
+        async {
+            let! r = Http.AsyncRequest("http://codingwithsam.com")
+            let count = 
+                match r.Body with 
+                | Text t -> t.Length 
+                | Binary _ -> 0
+            sprintf "%d" count |> Debug.WriteLine 
+        }
+
+    do 
+        clickMe.Clicked.Add (fun _ -> 
+            apiLookup () |> Async.RunSynchronously)
+
+    
